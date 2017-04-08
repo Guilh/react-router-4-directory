ca467dd08aa4b2f16c2d62389744196d0db40146
Guil Hernandez <g.hernandez07@gmail.com>
2017-04-07 22:20:11 -0400

    update PropTypes

--- src/components/courses/CourseContainer.js
+++ src/components/courses/CourseContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Course from './Course';
 
 const CourseContainer = (props) => {
@@ -18,5 +19,9 @@ const CourseContainer = (props) => {
     </div>
   );
 }
+
+CourseContainer.propTypes = {
+  data: PropTypes.array.isRequired     
+};
 
 export default CourseContainer;
